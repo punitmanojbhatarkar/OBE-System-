2f5aa3475dc526d88275d9935ea8a8c1a9bd0940
Punit Bhatarkar <punitbhatarkar650@gmail.com>
2026-09-21 20:35:25 +0530

    Fix CORS by allowing all origins for Vercel

--- backend/main.py
+++ backend/main.py
@@ -59,8 +59,8 @@ allowed_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
